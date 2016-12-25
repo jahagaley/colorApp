cbdf48e838df8eb007c42d5cf0380dc9420bf401
jahagaley <jahagaley@hotmail.com>
2016-12-24 16:24:59 -0800

    First Commit

---
+++ www/scripts/index.js
@@ -0,0 +1,18 @@
+﻿function upDateColor() {
+
+
+    document.body.style.backgroundColor = newColor();
+
+
+}
+function getRandomInt() {
+    return Math.floor(Math.random() * (255 + 1));
+}
+function newColor() {
+
+    var num1 = getRandomInt();
+    var num2 = getRandomInt();
+    var num3 = getRandomInt();
+
+    return "rgb(" + num1 + "," + num2 + "," + num3 + ")";
+}
